e7152e6a42da427eaf7c683213d547d9377722a6
Hanif Razin Rahmatullah <hanifrazin@gmail.com>
2018-10-05 01:00:49 +0700

    Upload Last golok_latih & golok_uji

--- uji_tes_golok5.m
+++ uji_tes_golok5.m
@@ -30,39 +30,17 @@ area = zeros(total_images,1);
 perimeter = zeros(total_images,1);
 diameter = zeros(total_images,1);
 ro = zeros(total_images,1);
-ro2 = zeros(total_images,1);
-ro3 = zeros(total_images,1);
 co = zeros(total_images,1);
-co2 = zeros(total_images,1);
-solidity = zeros(total_images,1);
-eccentricity = zeros(total_images,1);
-eccentricity2 = zeros(total_images,1);
-slim = zeros(total_images,1);
-rect = zeros(total_images,1);
-rect2 = zeros(total_images,1);
-narrow = zeros(total_images,1);
-rpd = zeros(total_images,1);
-prp = zeros(total_images,1);
-minor_axis = zeros(total_images,1);
-mayor_axis = zeros(total_images,1);
-elongation = zeros(total_images,1);
-convex_perim = zeros(total_images,1);
-convexity = zeros(total_images,1);
-conv_area = zeros(total_images,1);
-conv_hull = zeros(total_images,1);
 level = zeros(total_images,1);
 
-SE = ones(3);
-SE2 = [1 1 1;1 1 1;1 1 1]
+SE = [1 1 1;1 1 1;1 1 1];
+SE2 = [1 1 1;1 1 1;1 1 1];
 
 for n = 1:total_images
     full_name = fullfile(image_folder, filenames(n).name);
     Img = imread(full_name);
 
     Gray = rgb2gray(Img);
-%     Contrast = imadjust(Gray,stretchlim(Gray),[]);
-    
-%     MedImg = ordfilt2(Gray,5,ones(3,3));
     Crop = imcrop(Gray,[104.5 0.5 190 70]);
     Resize = imresize(Crop,[50 150]);
     
@@ -71,15 +49,15 @@ for n = 1:total_images
     level(n) = graythresh(Resize);
     Biner = not(imbinarize(Resize,0.8));
     
-    Open2 = imopen(Biner,SE2);
-    Close2 = imclose(Open2,SE2);
+    Open2 = imopen(Biner,SE);
+    Close2 = imclose(Open2,SE);
     Open = bwmorph(Biner,'open');
     Close = bwmorph(Open,'close');
     Fill = imfill(Close2,'holes');
         
-        if(n==28)
-           figure,imshow(Fill);title(filenames(n).name);
-        end
+%         if(n==28)
+%            figure,imshow(Fill);title(filenames(n).name);
+%         end
     
     conv = bwconvhull(Close);
     Label = bwlabel(Close);
@@ -87,57 +65,14 @@ for n = 1:total_images
     conv_stats = regionprops(conv,'Perimeter');
     area(n) = stats.Area;
     perimeter(n) = stats.Perimeter;
-    minor_axis(n) = stats.MinorAxisLength;
-    mayor_axis(n) = stats.MajorAxisLength;
-    convex_perim(n) = conv_stats.Perimeter;
-    convexity(n) = convex_perim(n)/perimeter(n);
     co(n) = (perimeter(n).^2)/area(n);
-    co2(n) = (perimeter(n).^2)/(4*pi*area(n));
-    conv_area(n) = stats.ConvexArea;
-    conv_hull(n) = conv_area(n)-area(n);
     ro(n) = (4*pi*area(n))/(perimeter(n).^2);
-    ro2(n) = (4*pi*area(n))/(convex_perim(n).^2);
-    ro3(n) = (4*area(n))/(pi*(mayor_axis(n).^2));
-    eccentricity(n) = stats.Eccentricity;
-    eccentricity2(n) = sqrt(((mayor_axis(n).^2)-(minor_axis(n).^2)))/mayor_axis(n);
-    solidity(n) = stats.Solidity;
-    diameter(n) = stats.EquivDiameter;
-    
-    prp(n) = perimeter(n)/(mayor_axis(n)+minor_axis(n));
-    narrow(n) = diameter(n)/mayor_axis(n);
-    slim(n) = mayor_axis(n)/minor_axis(n);
-    rpd(n) = perimeter(n)/mayor_axis(n);
-    
-    rect(n) = area(n)/(minor_axis(n)*mayor_axis(n));
-    rect2(n) = (minor_axis(n)*mayor_axis(n))/area(n);
-    elongation(n) = 1 - (minor_axis(n)/mayor_axis(n));
-    
 end
 
 area_uji = zeros(total_images_uji,1);
 perimeter_uji = zeros(total_images_uji,1);
-diameter_uji = zeros(total_images_uji,1);
 ro_uji = zeros(total_images_uji,1);
-ro2_uji = zeros(total_images_uji,1);
-ro3_uji = zeros(total_images_uji,1);
 co_uji = zeros(total_images_uji,1);
-co2_uji = zeros(total_images_uji,1);
-solidity_uji = zeros(total_images_uji,1);
-eccentricity_uji = zeros(total_images_uji,1);
-eccentricity2_uji = zeros(total_images_uji,1);
-slim_uji = zeros(total_images_uji,1);
-rect_uji = zeros(total_images_uji,1);
-rect2_uji = zeros(total_images_uji,1);
-narrow_uji = zeros(total_images_uji,1);
-rpd_uji = zeros(total_images_uji,1);
-prp_uji = zeros(total_images_uji,1);
-minor_axis_uji = zeros(total_images_uji,1);
-mayor_axis_uji = zeros(total_images_uji,1);
-elongation_uji = zeros(total_images_uji,1);
-convex_perim_uji = zeros(total_images_uji,1);
-convexity_uji = zeros(total_images_uji,1);
-conv_area_uji = zeros(total_images_uji,1);
-conv_hull_uji = zeros(total_images_uji,1);
 level_uji = zeros(total_images_uji,1);
 
 for n = 1:total_images_uji
@@ -145,17 +80,14 @@ for n = 1:total_images_uji
     Img_uji = imread(full_name_uji);
     
     Gray_uji = rgb2gray(Img_uji);
-%     Contrast_uji = imadjust(Gray_uji,stretchlim(Gray_uji),[]);
-    
-%     MedImg_uji = ordfilt2(Gray_uji,25,ones(7,7));
     Crop_uji = imcrop(Gray_uji,[104.5 0.5 190 70]);
     Resize_uji = imresize(Crop_uji,[50 150]); 
     
 %     Biner_uji = Resize_uji < 200;
 %     Biner_uji = not(Resize_uji > 200);
     level_uji(n) = graythresh(Resize_uji);
-    Biner_uji = not(imbinarize(Resize_uji));
-    bin = imbinarize(Resize_uji,0.8);
+    Biner_uji = not(imbinarize(Resize_uji,0.8));
+    bin = imbinarize(Resize_uji);
     Open_uji2 = imopen(Biner_uji,SE2);
     Close_uji2 = imclose(Open_uji2,SE2);
     Open_uji = bwmorph(Biner_uji,'open');
@@ -171,34 +103,10 @@ for n = 1:total_images_uji
 %     imwrite(Open_uji,path_uji,'jpg')
     conv_uji = bwconvhull(Close_uji);
     stats_uji = regionprops(Fill_uji,'all');
-    conv_stats_uji = regionprops(conv_uji,'Perimeter');
     area_uji(n) = stats_uji.Area;
     perimeter_uji(n) = stats_uji.Perimeter;
-    convex_perim_uji(n) = conv_stats_uji.Perimeter;
-    minor_axis_uji(n) = stats_uji.MinorAxisLength;
-    mayor_axis_uji(n) = stats_uji.MajorAxisLength;
-    convexity_uji(n) = convex_perim_uji(n)/perimeter_uji(n);
     co_uji(n) = (perimeter_uji(n).^2)/area_uji(n);
-    co2_uji(n) = (perimeter_uji(n).^2)/(4*pi*area_uji(n));
-    conv_area_uji(n) = stats_uji.ConvexArea;
-    conv_hull_uji(n) = conv_area_uji(n)-area_uji(n);
     ro_uji(n) = (4*pi*area_uji(n))/(perimeter_uji(n).^2);
-    ro2_uji(n) = (4*pi*area_uji(n))/(convex_perim_uji(n).^2);
-    ro3_uji(n) = (4*area_uji(n))/(pi*(mayor_axis_uji(n).^2));
-    eccentricity_uji(n) = stats_uji.Eccentricity;
-    eccentricity2_uji(n) = sqrt(((mayor_axis_uji(n).^2)-(minor_axis_uji(n).^2)))/mayor_axis_uji(n);
-    solidity_uji(n) = stats_uji.Solidity;
-    diameter_uji(n) = stats_uji.EquivDiameter;
-    
-    rpd_uji(n) = perimeter_uji(n)/mayor_axis_uji(n);
-    prp_uji(n) = perimeter_uji(n)/(mayor_axis_uji(n)+minor_axis_uji(n));
-    narrow_uji(n) = diameter_uji(n)/mayor_axis_uji(n);
-    
-    slim_uji(n) = mayor_axis_uji(n)/minor_axis_uji(n);
-
-    rect_uji(n) = area_uji(n)/(mayor_axis_uji(n)*minor_axis_uji(n));
-    rect2_uji(n) = (mayor_axis_uji(n)*minor_axis_uji(n))/area_uji(n);
-    elongation_uji(n) = 1 - (minor_axis_uji(n)/mayor_axis_uji(n));
 end
 
 % KLASIFIKASI MUTU MUTIARA BERDASARKAN BENTUK DAN UKURAN MENGGUNAKAN K-NEAREST NEIGHBOR
@@ -246,6 +154,34 @@ Akurasi_test_non = 100*(sum(diag(C_test_non))./sum(C_test_non(:)));
 Table_test_non = table(isBayes2(26:30),class_uji(26:30))
 disp(['accuracy Data uji Bayes Non Betawi = ',num2str(Akurasi_test_non,'%.2f'),'%'])
 toc;
+hasil = '';
+
+[j k] = size(posterior2);
+Poster_Bayes_Uji = zeros(j,1);
+for i=1:j
+    Poster_Bayes_Uji(i) = max(posterior2(i,:));
+end
+
+Result_Bayes = cell(total_images,1);
+for i=1:numel(isBayes2)
+    if isequal(isBayes2(i),1)
+        hasil = 'Gablogan';
+    elseif isequal(isBayes2(i),2)
+        hasil = 'Sembelih';
+    elseif isequal(isBayes2(i),3)
+        hasil = 'Sorenan';
+    elseif isequal(isBayes2(i),4)
+        hasil = 'Ujung Turun';
+    elseif isequal(isBayes2(i),5)
+        hasil = 'Petok';
+    elseif isequal(isBayes2(i),6)
+        hasil = 'Bukan Golok Betawi';
+    end
+    Result_Bayes{i} = hasil;
+end
+
+X=[uji Poster_Bayes_Uji];
+Y=[uji posterior2];
 
 T_Latih=transpose(latih);
 T_Uji = transpose(uji);
